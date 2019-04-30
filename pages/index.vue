b027a916128e3d657db044742f9c07965350e758
Sergey Ukolov <zezic51@yandex.ru>
2019-05-01 01:27:42 +0300

    Published docs.

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template lang='pug'>
+.index-page
+  article-page(
+    :markdown='markdown',
+    :crumbs='[]',
+    :modules='modules',
+    :relatedModules='modules'
+  )
+</template>
+
+<script>
+import ArticlePage from '~/components/ArticlePage'
+import modules from '~/lib/modules.js'
+
+export default {
+  components: {
+    ArticlePage
+  },
+  async asyncData ({ $axios, app, params }) {
+    const locale = app.i18n.locale
+    const path = `/markdown/index.${locale}.md`
+    const markdown = await $axios.$get(path)
+    return { markdown }
+  },
+  data: () => ({
+    modules
+  })
+}
+</script>
