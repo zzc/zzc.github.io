b8732611aa67e8a7434ee6471e21d2428fcd7fff
Sergey Ukolov <zezic51@yandex.ru>
2019-05-10 17:27:19 +0300

    Publish fresh docs.

--- sw.js
+++ sw.js
@@ -2,40 +2,40 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/0b5c360a018cab349837.js",
-    "revision": "a8c8a7d1952d88a96d0421206c411e38"
+    "url": "/_nuxt/30fe58f7ae87fed87e7b.js",
+    "revision": "5b5990732afc125fd8cd327464472719"
   },
   {
-    "url": "/_nuxt/252e739cd282def82f96.js",
-    "revision": "a9b11940fec0e3119fff38c166e1e45b"
+    "url": "/_nuxt/3e9de23bf9fe4299ca8d.js",
+    "revision": "12e28f59365dc28dfcb370ef5e1e818e"
   },
   {
-    "url": "/_nuxt/27e630975a0b02df964b.js",
-    "revision": "6c9169e9082ea645f5b158f85727b1c4"
+    "url": "/_nuxt/4d57ba6db037aaaea700.js",
+    "revision": "f0424e8c3d7cd1423a52ab8ae9d7e1a0"
   },
   {
-    "url": "/_nuxt/2867a6e3e79980f7eca3.js",
-    "revision": "45a1bf77aa34c31d81135c66eb69a4c7"
+    "url": "/_nuxt/6eb5a3f149476483179a.js",
+    "revision": "9ef62af25a3067e56424f9003e670c61"
   },
   {
-    "url": "/_nuxt/603f08d9c5bb55784f59.js",
-    "revision": "a49b0690024d3745cc53e7519dc0cfdc"
+    "url": "/_nuxt/865b3ad8c03f5de77ac7.js",
+    "revision": "7c5f35858cb011dffee3355807a8d9b6"
   },
   {
-    "url": "/_nuxt/96f9a8113cfe70fa718c.js",
-    "revision": "f23117e9f0fa40443447f0fd75dc4125"
+    "url": "/_nuxt/9fb32f67b12ad549237d.js",
+    "revision": "a12e5adc20d4551dbfd8bc3ac1070816"
   },
   {
-    "url": "/_nuxt/c252b57b2fe626df240b.js",
-    "revision": "2da3893d20d3ef0fdf11f34b0dcf239d"
-  },
-  {
-    "url": "/_nuxt/ce1d5cec6d44091e9c85.js",
-    "revision": "03a3d65e0765035e664d306670d8e596"
+    "url": "/_nuxt/aae77eb7561ff7bdb938.js",
+    "revision": "fac87d1444e7bc7f1c32b1d4dd3f3035"
   },
   {
     "url": "/_nuxt/cfed4ed80766c0c60950.js",
     "revision": "951da48dff40720fcadbc7a3062ba3c1"
+  },
+  {
+    "url": "/_nuxt/e44375e4d145c7254f00.js",
+    "revision": "62b59be54356351c9b1f4cd23682f9ff"
   }
 ], {
   "cacheId": "zzc-docs",
