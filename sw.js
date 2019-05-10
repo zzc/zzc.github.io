343595c10881afa052b279d2e0c3df5ee99f8590
Sergey Ukolov <zezic51@yandex.ru>
2019-05-10 17:55:55 +0300

    Update public site.

--- sw.js
+++ sw.js
@@ -2,40 +2,40 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/30fe58f7ae87fed87e7b.js",
-    "revision": "5b5990732afc125fd8cd327464472719"
+    "url": "/_nuxt/10392cc200d5bbd55f39.js",
+    "revision": "a3d0f7c7addb25b21146a5c315b50515"
   },
   {
-    "url": "/_nuxt/3e9de23bf9fe4299ca8d.js",
-    "revision": "12e28f59365dc28dfcb370ef5e1e818e"
+    "url": "/_nuxt/262b69c94417da4dc068.js",
+    "revision": "1a2be79d9ec844728509b6c7e852062d"
   },
   {
     "url": "/_nuxt/4d57ba6db037aaaea700.js",
     "revision": "f0424e8c3d7cd1423a52ab8ae9d7e1a0"
   },
   {
-    "url": "/_nuxt/6eb5a3f149476483179a.js",
-    "revision": "9ef62af25a3067e56424f9003e670c61"
+    "url": "/_nuxt/4f86d565d8152cef7625.js",
+    "revision": "62a195fd44a2dbfb642bda631bff1550"
   },
   {
-    "url": "/_nuxt/865b3ad8c03f5de77ac7.js",
-    "revision": "7c5f35858cb011dffee3355807a8d9b6"
+    "url": "/_nuxt/516363676f2dc6d8192f.js",
+    "revision": "0751f1db29378391131e9eec4c1ba422"
   },
   {
-    "url": "/_nuxt/9fb32f67b12ad549237d.js",
-    "revision": "a12e5adc20d4551dbfd8bc3ac1070816"
+    "url": "/_nuxt/82ea2e22b321b3480dcc.js",
+    "revision": "5ab9de96d0b2d20e7543723207fba442"
   },
   {
-    "url": "/_nuxt/aae77eb7561ff7bdb938.js",
-    "revision": "fac87d1444e7bc7f1c32b1d4dd3f3035"
+    "url": "/_nuxt/93d1dcbc40e4f090849c.js",
+    "revision": "351b0375b3fc414dfd8b82b7f6338c7f"
   },
   {
     "url": "/_nuxt/cfed4ed80766c0c60950.js",
     "revision": "951da48dff40720fcadbc7a3062ba3c1"
   },
   {
-    "url": "/_nuxt/e44375e4d145c7254f00.js",
-    "revision": "62b59be54356351c9b1f4cd23682f9ff"
+    "url": "/_nuxt/d727dd282e004aa11b2f.js",
+    "revision": "84287d2abce1a12f1b8951ecbfb2f525"
   }
 ], {
   "cacheId": "zzc-docs",
